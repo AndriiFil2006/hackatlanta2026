1f62bf010ed8a4a5f7632e5ea3cb2a2956e4bda2
AndriiFil2006 <filandrey06@gmail.com>
2026-08-23 10:43:43 -0400

    Dockerize application with Docker Compose

--- database/schema.sql
+++ database/schema.sql
@@ -100,3 +100,39 @@ VALUES (
   	12,
   	'https://m.media-amazon.com/images/G/01/apparel/rcxgs/tile._CB483369110_.gif'
 );
+
+CREATE TABLE trips(
+	trip_id SERIAL PRIMARY KEY,
+	
+	trip_name VARCHAR(50) NOT NULL, 
+	start_date DATE,
+	end_date DATE,
+	
+	owner_user_id INTEGER NOT NULL,
+	destination_text TEXT,
+	destination_lat DOUBLE PRECISION,
+	destination_lng DOUBLE PRECISION,
+	
+	FOREIGN KEY (owner_user_id) REFERENCES users(user_id),
+	
+	CHECK (end_date IS NULL OR start_date IS NULL OR end_date >= start_date)
+);
+
+CREATE TABLE roles(
+	role_id SERIAL PRIMARY KEY,
+	role_name VARCHAR(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE memberships(
+	user_id INTEGER,
+	trip_id INTEGER,
+	
+	role_id INTEGER NOT NULL,
+	joined_at DATE DEFAULT CURRENT_DATE,
+	
+	PRIMARY KEY(user_id, trip_id),
+	
+	FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+	FOREIGN KEY (trip_id) REFERENCES trips(trip_id) ON DELETE CASCADE,
+	FOREIGN KEY (role_id) REFERENCES roles(role_id)
+);
